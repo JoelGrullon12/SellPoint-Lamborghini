b48702ee85cf4089f9b63e101701a8f82e019776
JoelGrullon12 <jjj007507388@gmail.com>
2022-04-24 03:40:27 -0400

    disenador de tipos entidades, olvide guardarlo XD

--- CapaPresentación/Menu Principal/Tipos Entidades.Designer.cs
+++ CapaPresentación/Menu Principal/Tipos Entidades.Designer.cs
@@ -250,6 +250,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(582, 335);
             this.dataGridView1.TabIndex = 48;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btnrefrescar
             // 
